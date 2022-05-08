81124061cc5a0a3fe07f6561b115a8d34ab44ec4
Kevin Rifel Kaiser <kevinrifelkaiser@gmail.com>
2022-05-08 19:57:42 -0300

    Set a chained route with get, post and delete routes

--- app.js
+++ app.js
@@ -22,45 +22,49 @@ const wikiSchema = new mongoose.Schema ({
 //Mongoose model
 const Article = mongoose.model('Article', wikiSchema);
 
-//REST API: Get rout
-app.get('/articles', function(req, res) {
-    Article.find({}, function(err, foundArticles) {
-        if(!err) {
-            res.send(foundArticles);
-        } else {
-            res.send(err);
-        }
+app.route('/articles')
+    .get(function(req, res) {
+        Article.find({}, function(err, foundArticles) {
+            if(!err) {
+                res.send(foundArticles);
+            } else {
+                res.send(err);
+            }
+        });
+    })
+    .post(function(req, res) {
+
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        });
+        
+        newArticle.save(function(err) {
+            if(!err) {
+                res.send('Successfully added a new article.')
+            } else {
+                res.send(err);
+            }
+        });
+    })
+    .delete(function(req, res) {
+        Article.deleteMany(function(err) {
+            if(!err) {
+                res.send('Successfully deleted all articles.');
+            } else {
+                res.send(err);
+            }
+        });
     });
-});
+
+//REST API: Get rout
+app.get('/articles', );
 
 //REST API: Post rout
-app.post('/articles', function(req, res) {
-
-    const newArticle = new Article({
-        title: req.body.title,
-        content: req.body.content
-    });
-    
-    newArticle.save(function(err) {
-        if(!err) {
-            res.send('Successfully added a new article.')
-        } else {
-            res.send(err);
-        }
-    });
-
-}); 
+app.post('/articles', ); 
 
 //REST API: Delete rout
-app.delete('/articles' , function(req, res) {
-    Article.deleteMany(function(err) {
-        if(!err) {
-            res.send('Successfully deleted all articles.');
-        } else {
-            res.send(err);
-        }
-    });
-});
+app.delete('/articles' , );
 
 
 
