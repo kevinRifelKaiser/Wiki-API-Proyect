9e274ebc5ac7bfe7a05ba7d9746edbd02100e0fc
Kevin Rifel Kaiser <kevinrifelkaiser@gmail.com>
2022-05-08 21:25:03 -0300

    Set individual RESTful delete route

--- app.js
+++ app.js
@@ -94,12 +94,19 @@ app.route('/articles/:articleTitle')
             }
         }
     );
+})
+.delete(function(req, res) {
+    Article.deleteOne(
+        {title: req.params.articleTitle},
+        function(err) {
+            if(!err) {
+                res.send('Successfully deleted article.');
+            } else {
+                res.send(err);
+            }
+        }
+    );
 });
-
-
-
-
-
 
 
 
